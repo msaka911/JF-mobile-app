582b1aae2a0fe93e684081316c1b52588c94d96a
Michael Xiang <14zx4@queensu.ca>
2022-09-16 16:28:41 -0400

    modify claim details

--- src/screens/ClaimPage.js
+++ src/screens/ClaimPage.js
@@ -1,21 +1,85 @@
 import * as React from 'react';
-import { Text, StyleSheet, View, Image,Button, TouchableOpacity } from 'react-native';
-import { useState } from 'react';
+import { Text, StyleSheet, View, Image, TouchableOpacity,LayoutAnimation } from 'react-native';
+import { useState, useEffect } from 'react';
 import {REACT_APP_BACKEND} from '@env'
+import { CustomLayoutSpring } from "react-native-animation-layout";
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 
 import Spacer from '../Spacer';
 import { Input } from 'react-native-elements';
 
 const ClaimPage=({route,navigation})=>{
-    
+    const[claimData, setData]=useState();
+    const[showDetail, setShow]=useState(false);
+
     const { data } = route.params;
+    const {api_id}=route.params;
+
+    var parsed= JSON.parse(data)
     
-    console.log(data)
+    useEffect(()=>{
+        var formdata = new FormData();
+        formdata.append('api_id', api_id);
+        formdata.append('token', parsed.token);
+    
+        var requestOptions = {
+            method: 'POST',
+            body: formdata,
+            redirect: 'follow'
+        };
+        
+        fetch("https://claim.otcww.com/Api/claims", requestOptions)
+            .then(response => response.text())
+            .then((result) => {
+                setData(JSON.parse(result))
+                LayoutAnimation.configureNext(CustomLayoutSpring());
+            })
+            .catch(error => console.log('error', error));
+    },[])
+   if(claimData){
+    for(const claim in claimData.claims){
+        console.log(claim)
+    }
+   }
 
     return(
-        <View >
+        <View>
+            {claimData?.claims.map((item)=>{
+            return(
+                <Card key={item.eclaim_no} mode="outlined">
+                <Card.Content>
+                <Title>{item.eclaim_no||" "}</Title>
+                <Paragraph>Claimed Amount: ${item.billed||" "}</Paragraph>
+                <Paragraph>Status: {item.status||" "}</Paragraph>
+                </Card.Content>
+                <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+                <Card.Actions>
+                <Button  onPress={(item)=>{setShow(!showDetail)
+              
+                }}>Details</Button>
+                <Button>Ok</Button>
+                {showDetail?
+                    item.items.map((claimItem)=>{
+                        <Card key={claimItem.claim_item_no} mode="outlined">
+                        <Card.Content>
+                        <Title>{claimItem.diagnosis}</Title>
+                        <Paragraph>Claimed Amount: ${claimItem.amount_billed}</Paragraph>
+                        <Paragraph>Amount Payable: ${claimItem.amt_payable}</Paragraph>
+                        <Paragraph>Status: {claimItem.status}</Paragraph>
+                        <Paragraph>{claimItem.reason}</Paragraph>
+                        </Card.Content>
+                        </Card>
+                    })
+                    :null}
+                </Card.Actions>
+          </Card>
+            )
+
+            })}
+            
+        
       </View>
     )
 }
 
-export default ClaimPage
+export default ClaimPage;
--- src/screens/WebView.js
+++ src/screens/WebView.js
@@ -2,41 +2,74 @@ import { WebView } from 'react-native-webview';
 import * as React from 'react';
 import { useId } from 'react'
 
-import { Text, StyleSheet, View, Image,Button, TouchableOpacity } from 'react-native';
+import { Text, StyleSheet, View, Image,Button, TouchableOpacity,Alert } from 'react-native';
 import { useState } from 'react';
 import {REACT_APP_BACKEND} from '@env'
+import uniqueId from 'lodash/uniqueId'
 
 import Spacer from '../Spacer';
 import { Input } from 'react-native-elements';
+
 
 const Webview=({navigation})=>{
     const [policy_number,setPolicy]=useState("")
     const [birthday,setBirthday]=useState("")
 
-    const axios = require('axios');
-    const id = useId();
+    const id = uniqueId('jfinsurancepolicy')
+    var axios = require('axios');
+    var FormData = require('form-data');
 
+    // const onButtonClick=()=>{
+        
+    //     if(policy_number&&birthday){ 
+        
+    //         var data = new FormData();
+    //         data.append('policy', 'JFPL658585');
+    //         data.append('birthday', '1998-02-16');
+    //         data.append('api_id', 'jfinsurance25');
+    //         // data.append('policy', policy_number);
+    //         // data.append('birthday', birthday);
+    //         // data.append('api_id', id);
+    //         var config = {
+    //             method: 'post',
+    //             url: 'https://claim.otcww.com/Api/login',
+    //             data : data
+    //           };
+
+    //       axios(config
+    //       ).then((response)=>{
+    //         Alert.alert("Successful",",[]")
+    //          navigation.navigate("ClaimPage",{ data: response})
+    //         }
+    //       ).catch(error=>{
+    //         Alert.alert('Authentication Failed, Please check your policy number or birthday',"",[])
+    //         console.log(error.response)
+    //       })
+    //     }
+    //     else{
+    //       Alert.alert('Please input valid information','',[])
+    //     }
+    // }
     const onButtonClick=()=>{
-        if(policy_number&&birthday){
-          axios({
-            method: 'post',
-            url: "https://claim.otcww.com/Api/login",
-            data: {
-              policy:policy_number,
-              birthday:birthday,
-              api_id: id
-            }
-          }).then((data)=>{
-             navigation.navigate("ClaimPage",{ data: data})
-            }
-          ).catch(error=>{
-            Alert.alert('Authentication Failed, Please check your policy number or birthday','Tyr it later',[])
-          })
-        }
-        else{
-          Alert.alert('Please input valid information','',[])
-        }
+
+        
+        var formdata = new FormData();
+        formdata.append('policy', policy_number);
+        formdata.append('birthday', birthday);
+        formdata.append('api_id', id);
+        
+        var requestOptions = {
+          method: 'POST',
+          body: formdata,
+          redirect: 'follow'
+        };
+        
+        fetch("https://claim.otcww.com/Api/login", requestOptions)
+          .then(response => response.text())
+          .then(result => navigation.navigate("ClaimPage",{ data: result,api_id:id}))
+          .catch(error => console.log('error', error));
     }
+
     const styles = StyleSheet.create({
         viewStyle:{
             alignItems:'center',
